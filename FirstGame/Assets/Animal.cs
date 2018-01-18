8963f836128238ea70e8937a61fd2122c40a0984
Unknown <merryannnguyen@yahoo.com>
2018-01-18 11:17:03 -0700

    Worked on Animal class
    added some funtions

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+//Remember that the name of the class has to match the file 
+//in a code block {} are needed, the end of the line needs ; print is a funtion that needs ()
+	// Use this for initialization
+	void Start () {
+
+		print("I love my dog.");
+		print("Yes, I really do.");
+	}
+void OnMouseDown()
+{
+	print("Bark.");
+}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
