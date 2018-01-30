5323811e68e9761e58e184a259f7f5ae21d610a5
Unknown <merryannnguyen@yahoo.com>
2018-01-30 11:18:37 -0700

    updated unity to have public bool and key

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -3,20 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Animal : MonoBehaviour {
-//Remember that the name of the class has to match the file 
-//in a code block {} are needed, the end of the line needs ; print is a funtion that needs ()
-	// Use this for initialization
-	void Start () {
 
-		print("I love my dog.");
-		print("Yes, I really do.");
-	}
+public bool Key = false;
 void OnMouseDown()
 {
-	print("Bark.");
+	if(Key == true)
+	print("Open Seseme");
 }
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
