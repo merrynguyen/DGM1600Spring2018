56f13299cbcb7cce56a032a15d574170e6c2f287
Unknown <merryannnguyen@yahoo.com>
2018-02-08 10:17:39 -0700

    updated first game with power up, math operators, logical operators

---
+++ FirstGame/Assets/ClickPowerUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickPowerUp : MonoBehaviour {
+	public PowerUp MyPowerUp;
+	void OnMouseDown()
+	{
+		if (MyPowerUp.PowerLevel >= 100)
+	{
+		print("I Have the Power!!!");
+	}	
+	}
+}
---
+++ FirstGame/Assets/LogicOperators.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicOperators : MonoBehaviour {
+	public int Heart;
+	public int Health;
+	public string Password = "Love";
+
+
+
+	// Use this for initialization
+	void Start () {
+		if (Password == "Love" || Password == "LOVE" || Password =="luv")
+		{
+			Heart = 100;
+			Health += 10;
+		}	
+	
+	}
+}
