c6c61b736c6b9196f9d9d27fac4d295811060cd2
Unknown <merryannnguyen@yahoo.com>
2018-03-08 10:33:49 -0700

    updated calculation

---
+++ FirstGame/Assets/Scripts/Calculator/Calculation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Calculation : ScriptableObject {
+	public int Calculate()
+	{
+		return 10;
+	}
+
+}
